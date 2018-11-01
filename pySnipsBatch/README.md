69f0fdc3115fe2fc482f6c0d69a6f21a847be8cf
KiboOst <KiboOst@users.noreply.github.com>
2018-11-01 11:55:26 +0100

    Add files via upload

---
+++ pySnipsBatch/README.md
@@ -0,0 +1,108 @@
+# SNIPS Batch Tester
+
+The SNIPS console allow sentence testing, but only one by one, display intent and slot.
+Here is a python script that allow to test a batch of sentences in one go, each with the intent it should match !
+Why this tool ? Actually, the SNIPS NLU is quiet limited. For example, you can have a sentence matching the right intent, retrain your assistant with exact same configuration, and same sentence doesn't match anymore. This is due to several limitations in the NLU:
+
+- SNIPS NLU is bag of word type, so no semantic is taken into account.
+- Not all training sentences are taken each time for performance reason, which insert random behavior.
+- It do words simplification, two words which are almost the same can be seen as the same by the NLU.
+
+So, each time I change something in slots or intent training sentences, I had to try a lot of sentences one by one with copy/paste to be sure not all was over. Here came the idea of batch testing sentences in one go.
+
+Basically, this python script will connect to SNIPS console with your credentials, and send request with sentence to test, just like you do by hand on the console. It will then read and parse the result and display it.
+Here is a result example:
+
+> [Intent MATCHED] "éteins la lumière de la cuisine" | intentName: kiboost:lightsTurnOffJeedom | 0.69531906
+slotName: house_room | entity: house_room | rawValue: cuisine | value: cuisine
+
+> [!!Intent UNMATCHED!!] "maman aura quel age dans deux ans et demi" | intentName: kiboost:getAge | 0.50317174
+slotName: forename | entity: forename | rawValue: maman | value: aurelie
+slotName: age | entity: snips/number | rawValue: deux | value: 2
+
+Here is the batch.json file corresponding to test these two sentences:
+```json
+{
+	"éteins la lumière de la cuisine": "kiboost:lightsTurnOffJeedom ",
+	"maman aura quel age dans deux ans et demi": "kiboost:getNextAge"
+}
+```
+### How to
+
+- Download files batch.json and pySnipsBatcher.py in same folder.
+- Edit batch.json with your sentences and intent name it should match (or let '').
+- Edit pySnipsBatcher.py to put your SNIPS console credentials into ```if __name__ == "__main__":```
+
+```
+> You can find your project ID (proj_xxxx) in your url when connected on your console assistant. Ex: https://console.snips.ai/assistants/proj_xxxx
+```
+
+You can either enter sentences directly in the script:
+
+```python
+if __name__ == "__main__":
+	#snips console credentials and project:
+	login = "myloginemail"
+	password = "mysnipspass"
+	project = "proj_xxxxxx"
+
+	#connect to your console:
+	snips = snipsBatcher(login, password, project)
+
+	#test single sentence on console:
+	result = snips.testQuery("ouvre les volets")
+
+	#display result with intent that should matches:
+	snips.showResult(result, 'kiboost:OpenCoverJeedom')
+```
+
+Or use the batch.json file:
+```python
+if __name__ == "__main__":
+	#snips console credentials and project:
+	login = "myloginemail"
+	password = "mysnipspass"
+	project = "proj_xxxxxx"
+
+	#connect to your console:
+	snips = snipsBatcher(login, password, project)
+
+	#load batch.json file to test:
+	batchJson = json.load(open('batch.json', 'rb'), encoding='utf-8')
+
+	for item in batchJson:
+		if _debug: debug(3, 'batch test: %s | %s'%(item, batchJson[item]))
+		result = snips.testQuery(item)
+		snips.showResult(result, batchJson[item])
+```
+
+Then, just run pySnipsBatcher.py file and read output `:stuck_out_tongue_winking_eye:`
+
+-----------------
+#### 2018-11-01
+- First public version.
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2018 KiboOst
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ pySnipsBatch/pySnipsBatcher.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+import sys, os
+
+import json
+import urllib
+
+import urllib.request
+requestUrl = urllib.request
+parseUrl = urllib.parse
+from http.cookiejar import CookieJar, LWPCookieJar
+
+try:
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+except:
+	pass
+
+_debug = False
+#_debug = 3
+
+class snipsBatcher():
+
+	def testQuery(self, inputQuery):
+		jsonObj = {
+					"operationName": "NluInferenceQuery",
+					"variables": {
+						"assistantId": self._proj,
+						"query": inputQuery
+					},
+					"query": "query NluInferenceQuery($assistantId: ID!, $query: String!) {\n  nluInference(assistantId: $assistantId, query: $query) {\n    input\n    status\n    intent {\n      intentName\n      probability\n      __typename\n    }\n    slots {\n      rawValue\n      value\n      range {\n        start\n        end\n        __typename\n      }\n      entity\n      slotName\n      __typename\n    }\n    __typename\n  }\n}\n"
+				}
+
+		jsonString = json.dumps(jsonObj)
+		jsonString = '[' + jsonString + ']'
+		answer = self.request('POST', self._urlHost, '/gql', jsonString)
+		if _debug: debug(3, "testQuery:answer %s"%answer)
+		return answer[0]['data']['nluInference']
+	#
+
+	def showResult(self, nluInference, matchIntent=''):
+		displayResult = ''
+
+		intentInput = nluInference['input']
+
+		#no intent found:
+		if not 'intentName' in nluInference['intent']:
+			displayResult = '[!!No intent found!!] %s'%intentInput
+			if matchIntent != '':
+				displayResult += ' should match: %s'%matchIntent
+			print(displayResult)
+			return False
+
+		intentName = nluInference['intent']['intentName']
+		intentProba = nluInference['intent']['probability']
+		slots = nluInference['slots']
+
+		if matchIntent != '':
+			if matchIntent == intentName:
+				displayResult += '[Intent MATCHED]'
+			else:
+				displayResult += '[!!Intent UNMATCHED!!]'
+
+		displayResult += ' "%s" | intentName: %s | %s'%(intentInput, intentName, intentProba)
+		print(displayResult)
+		for slot in slots:
+			slotName = slot['slotName']
+			entity = slot['entity']
+			rawValue = slot['rawValue']
+			try:
+				value = json.loads(slot['value'])['value']
+			except:
+				value = slot['value']
+			displaySlotResult = 'slotName: %s | entity: %s | rawValue: %s | value: %s'%(slotName, entity, rawValue, value)
+			print(displaySlotResult)
+		if len(slots) == 0:
+			print('No slot found')
+		print()
+	#
+
+	def request(self, method, host, path='', jsonString=None, postinfo=None): #standard function handling all get/post request
+		if self._reqHdl == None:
+			self._reqHdl = requestUrl.build_opener(requestUrl.HTTPCookieProcessor(self.cookieJar))
+			self._reqHdl.addheaders = [
+						('User-agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0'),
+						('Connection', 'keep-alive'),
+						('Referer', self._urlHost+'/login'),
+						('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'),
+						('Upgrade-Insecure-Requests', 1)
+					]
+
+		url = host+path
+		if _debug: debug(4, 'request: %s method: %s postinfo: %s'%(url, method, postinfo))
+
+		if method == 'GET':
+			answer = self._reqHdl.open(url, timeout = 5)
+		else:
+			if jsonString != None:
+				jsonBytes = jsonString.encode()
+				req = urllib.request.Request(url, data=jsonBytes, headers={'Content-Type': 'application/json'})
+				answer = self._reqHdl.open(req)
+
+			if postinfo != None:
+				data = parseUrl.urlencode(postinfo)
+				if sys.version_info[0] == 3: data = data.encode()
+				answer = self._reqHdl.open(url, data, timeout = 5)
+
+		self.cookieJar.save(self._cookFile, ignore_discard=True)
+
+		if jsonString != None:
+			if _debug: debug(5, 'request info: %s'%answer.info())
+			result = json.load(answer)
+			return result
+
+		return answer.read()
+	#
+
+	def connect(self):
+		postinfo = {'email': self._login, 'password': self._password}
+		answer = self.request('POST', self._urlHost, '/api/login', None, postinfo)
+		if _debug: debug(4, 'answer connect: %s'%answer)
+		if b'Wake word' in answer:
+			return True
+		return False
+	#
+
+	def __init__(self, login='', password='', project=''):
+		self._version = 0.1
+		self.error = None
+
+		self._login = login
+		self._password = password
+		self._proj = project
+
+		self._urlHost = 'https://console.snips.ai'
+		self._reqHdl = None
+		self.cookieJar = LWPCookieJar()
+		self._cookFile = ''
+
+		if os.access('/', os.W_OK):
+			APIfolder = os.path.dirname(os.path.realpath(__file__))
+			self._cookFile = APIfolder+'/snipsBatcher_cookies.txt'
+
+		if self.connect() == True:
+			if _debug: debug(1, "__SNIPS console connected__")
+	#
+
+
+def debug(level, text):
+	if _debug >= level:
+		print("--debug:", text)
+#
+
+
+#testing purpose:
+if __name__ == "__main__":
+	#snips console credentials and project:
+	login = "myloginemail"
+	password = "mysnipspass"
+	project = "proj_xxxxxx"
+
+	#connect to your console:
+	snips = snipsBatcher(login, password, project)
+
+	"""
+	#test single sentence on console:
+	result = snips.testQuery("ouvre les volets")
+
+	#display result with intent that should matches:
+	snips.showResult(result, 'kiboost:OpenCoverJeedom')
+	"""
+
+	#load batch.json file to test:
+	batchJson = json.load(open('batch.json', 'rb'), encoding='utf-8')
+
+	for item in batchJson:
+		if _debug: debug(3, 'batch test: %s | %s'%(item, batchJson[item]))
+		result = snips.testQuery(item)
+		snips.showResult(result, batchJson[item])
